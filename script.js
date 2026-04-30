c189ab75333095357dc056c391a492f6b54d4fdc
blackzww <143085633+blackzww@users.noreply.github.com>
2026-04-30 20:00:37 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// BOTÃO DE COMPRA
 function comprar() {
     window.location.href = "https://wa.me/SEUNUMERO";
 }
+
+// BUSCA
+const search = document.getElementById("search");
+const cards = document.querySelectorAll(".card");
+
+search.addEventListener("input", () => {
+    const value = search.value.toLowerCase();
+
+    cards.forEach(card => {
+        const name = card.dataset.name;
+
+        if (name.includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
